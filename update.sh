f790b3d8fd4be6078339d62a092371b0f3c48486
Thibaud ROBIN <thibaud.robin@trackflaw.com>
2025-08-05 13:11:42 +0200

    fix: avoid force pushing conflict

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
 git fetch upstream
 git merge upstream/main
+
+if [ $? -ne 0 ]; then
+    echo "Conflit détecté lors de la fusion. Veuillez résoudre les conflits avant de continuer."
+    exit 1
+fi
+
 git push origin main --force
